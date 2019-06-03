f09f2ec6967a2b9615983e3ee8c05672b7679e97
toanGa <trantoan20122582@gmail.com>
2019-06-04 00:41:42 +0700

    init commit

---
+++ OMAPSendImage/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace OMAPSendImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonGetImage = new System.Windows.Forms.Button();
+            this.serialPortOmap = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxOmapCOM = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSendImage = new System.Windows.Forms.Button();
+            this.textBoxLinkIMG = new System.Windows.Forms.TextBox();
+            this.buttonOpenPort = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxLOG = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(260, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 320);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonGetImage
+            // 
+            this.buttonGetImage.Location = new System.Drawing.Point(22, 148);
+            this.buttonGetImage.Name = "buttonGetImage";
+            this.buttonGetImage.Size = new System.Drawing.Size(175, 62);
+            this.buttonGetImage.TabIndex = 1;
+            this.buttonGetImage.Text = "Get Image";
+            this.buttonGetImage.UseVisualStyleBackColor = true;
+            this.buttonGetImage.Click += new System.EventHandler(this.buttonGetImage_Click);
+            // 
+            // serialPortOmap
+            // 
+            this.serialPortOmap.BaudRate = 115200;
+            this.serialPortOmap.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPortOmap_DataReceived);
+            // 
+            // comboBoxOmapCOM
+            // 
+            this.comboBoxOmapCOM.FormattingEnabled = true;
+            this.comboBoxOmapCOM.Location = new System.Drawing.Point(17, 38);
+            this.comboBoxOmapCOM.Name = "comboBoxOmapCOM";
+            this.comboBoxOmapCOM.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxOmapCOM.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "COM";
+            // 
+            // buttonSendImage
+            // 
+            this.buttonSendImage.Location = new System.Drawing.Point(22, 234);
+            this.buttonSendImage.Name = "buttonSendImage";
+            this.buttonSendImage.Size = new System.Drawing.Size(175, 62);
+            this.buttonSendImage.TabIndex = 4;
+            this.buttonSendImage.Text = "Send Image";
+            this.buttonSendImage.UseVisualStyleBackColor = true;
+            this.buttonSendImage.Click += new System.EventHandler(this.buttonSendImage_Click);
+            // 
+            // textBoxLinkIMG
+            // 
+            this.textBoxLinkIMG.Location = new System.Drawing.Point(260, 37);
+            this.textBoxLinkIMG.Name = "textBoxLinkIMG";
+            this.textBoxLinkIMG.Size = new System.Drawing.Size(240, 22);
+            this.textBoxLinkIMG.TabIndex = 5;
+            this.textBoxLinkIMG.Text = "C:\\Users\\thuyd\\Desktop\\Apple2.jpg";
+            // 
+            // buttonOpenPort
+            // 
+            this.buttonOpenPort.Location = new System.Drawing.Point(17, 68);
+            this.buttonOpenPort.Name = "buttonOpenPort";
+            this.buttonOpenPort.Size = new System.Drawing.Size(121, 34);
+            this.buttonOpenPort.TabIndex = 6;
+            this.buttonOpenPort.Text = "Open";
+            this.buttonOpenPort.UseVisualStyleBackColor = true;
+            this.buttonOpenPort.Click += new System.EventHandler(this.buttonOpenPort_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Chartreuse;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonOpenPort);
+            this.panel1.Controls.Add(this.comboBoxOmapCOM);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(185, 113);
+            this.panel1.TabIndex = 7;
+            // 
+            // textBoxLOG
+            // 
+            this.textBoxLOG.Location = new System.Drawing.Point(547, 80);
+            this.textBoxLOG.Multiline = true;
+            this.textBoxLOG.Name = "textBoxLOG";
+            this.textBoxLOG.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLOG.Size = new System.Drawing.Size(272, 318);
+            this.textBoxLOG.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 436);
+            this.Controls.Add(this.textBoxLOG);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBoxLinkIMG);
+            this.Controls.Add(this.buttonSendImage);
+            this.Controls.Add(this.buttonGetImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonGetImage;
+        private System.IO.Ports.SerialPort serialPortOmap;
+        private System.Windows.Forms.ComboBox comboBoxOmapCOM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSendImage;
+        private System.Windows.Forms.TextBox textBoxLinkIMG;
+        private System.Windows.Forms.Button buttonOpenPort;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxLOG;
+    }
+}
+
---
+++ OMAPSendImage/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OMAPSendImage
+{
+    public partial class Form1 : Form
+    {
+        enum Command
+        {
+            CMD_NON,
+            CMD_SEND,
+            CMD_RECV,
+        }
+
+
+        ManualResetEvent RecvEvent = new ManualResetEvent(false);
+        ConcurrentQueue<Byte[]> mQueueRecv = new ConcurrentQueue<Byte[]>();
+        BackgroundWorker Worker = new BackgroundWorker();
+        Bitmap OmapBMP = new Bitmap(240, 320, PixelFormat.Format24bppRgb);
+        Command mCommand;
+    
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] ports = SerialPort.GetPortNames();
+            comboBoxOmapCOM.DataSource = ports;
+
+            if (ports.Length > 0)
+            {
+                comboBoxOmapCOM.Text = ports[0];
+            }
+
+            Worker.WorkerReportsProgress = true;
+            Worker.WorkerSupportsCancellation = true;
+            Worker.DoWork += delegate
+            {
+                HandlerSerialRecev();
+            };
+            Worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(WaitWorkerComplete);
+        }
+
+        private void WaitWorkerComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Worker Complete");
+            
+        }
+
+        void HandlerSerialRecev()
+        {
+            byte[] deqArr;
+            int bytesRecv;
+            int i;
+            int row = 0;
+            int col = 0;
+
+            serialPortOmap.Write("START_IMG");
+
+            while(true)
+            {
+                TraceLog("Receiver Wait");
+                RecvEvent.Reset();
+                RecvEvent.WaitOne();
+
+                bool status = mQueueRecv.TryDequeue(out deqArr);
+
+                if (status)
+                {
+                    bytesRecv = deqArr.Length;
+                    for(i = 0; i < bytesRecv; i += 3)
+                    {
+                        OmapBMP.SetPixel(row, col, Color.FromArgb(deqArr[i], deqArr[i + 1], deqArr[i + 2]));
+
+                        col++;
+                        if(col >= 240)
+                        {
+                            col = 0;
+                            row++;
+                            if(row == 320)
+                            {
+                                // Finish Receive sequence
+                                return;
+                            }
+                        }
+                    }
+                    TraceLog("Receiver: Write SEND_ME");
+                    serialPortOmap.Write("SEND_ME");
+                }
+            }
+        }
+
+        void TraceLog(string log)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke((MethodInvoker)delegate
+                {
+                    textBoxLOG.AppendText(log + "\r\n");
+                });
+            }
+            else
+            {
+                textBoxLOG.AppendText(log + "\r\n");
+            }
+        }
+
+        private void serialPortOmap_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort comport = sender as SerialPort;
+
+            if(mCommand == Command.CMD_RECV)
+            {
+                int bytes = comport.BytesToRead;
+                byte[] buffer = new byte[bytes];
+                comport.Read(buffer, 0, bytes);
+
+                mQueueRecv.Enqueue(buffer);
+                TraceLog("Receiver: Set");
+                RecvEvent.Set();
+            }
+            else if(mCommand == Command.CMD_SEND)
+            {
+                string content = comport.ReadExisting();
+                if(content == "SEND_ME" || true)
+                {
+                    TraceLog("SEND_ME received");
+                    TraceLog("Sender: Set");
+                    RecvEvent.Set();
+                }
+            }
+
+        }
+
+        private void buttonGetImage_Click(object sender, EventArgs e)
+        {
+            mCommand = Command.CMD_RECV;
+
+            if (!Worker.IsBusy)
+            {
+                Worker.RunWorkerAsync();
+            }
+        }
+
+        private void buttonSendImage_Click(object sender, EventArgs e)
+        {
+            string imgPath = textBoxLinkIMG.Text;
+
+            Bitmap img = (Bitmap)Image.FromFile(imgPath);
+            Color color;
+            int row, col;
+            int i, j;
+            byte[] sendPixel = new byte[3];
+
+            mCommand = Command.CMD_SEND;
+
+            row = img.Height;
+            col = img.Width;
+            
+            for(i = 0; i < row; i++)
+            {
+                for(j = 0; j < col; j++)
+                {
+                    color = img.GetPixel(i, j);
+                    sendPixel[0] = color.R;
+                    sendPixel[1] = color.G;
+                    sendPixel[2] = color.B;
+
+                    serialPortOmap.Write(sendPixel, 0, sendPixel.Length);
+
+                    TraceLog("Send " + i + "\r\n");
+
+                    if (i < row - 1)
+                    {
+                        TraceLog("Sender: Wait");
+                        RecvEvent.Reset();
+                        RecvEvent.WaitOne();
+                    }
+                }
+
+
+            }
+        }
+
+        private void buttonOpenPort_Click(object sender, EventArgs e)
+        {
+            if(!serialPortOmap.IsOpen)
+            {
+                serialPortOmap.PortName = comboBoxOmapCOM.Text;
+                serialPortOmap.Open();
+            }
+            else
+            {
+                MessageBox.Show("Serial port is open in another process");
+            }
+        }
+    }
+}
